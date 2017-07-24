cd810a5b7443c62c04fc8a51facd4de588e62ff2
Dinesh V <dinesh.varadharajan@relevancelab.com>
2017-07-24 18:00:28 +0530

    foodcritic error solved

--- new/metadata.rb
+++ new/metadata.rb
@@ -6,8 +6,11 @@ description 'Installs/Configures new'
 long_description 'Installs/Configures new'
 version '0.1.1'
 depends 'wordpress', '3.0.0'
+chef_version '>= 12.5' if respond_to?(:chef_version)
 # depends 'wordpress'
+supports 'redhat'
 
+opensource license supermarket
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
