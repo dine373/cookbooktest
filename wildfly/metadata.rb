787f9cc39b12cf8289d335b9b2dac53d0b8245b7
Dinesh V <dinesh.varadharajan@relevancelab.com>
2017-07-24 16:53:16 +0530

    commented metadata in two cookbook to test

--- wildfly/metadata.rb
+++ wildfly/metadata.rb
@@ -15,3 +15,7 @@ depends 'java', '~> 1.22'
 
 source_url 'https://github.com/bdwyertech/chef-wildfly' if respond_to?(:source_url)
 issues_url 'https://github.com/bdwyertech/chef-wildfly/issues' if respond_to?(:issues_url)
+
+
+
+# modifying to test
