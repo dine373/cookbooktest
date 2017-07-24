8880fcf927b2b06c084ad8c64d5420c6d5c68d89
Dinesh V <dinesh.varadharajan@relevancelab.com>
2017-07-24 13:08:03 +0530

    initial commit with cookbooks

---
+++ wildfly/attributes/postgres_datasources.rb
@@ -0,0 +1,24 @@
+default['wildfly']['postgresql']['jndi']['datasources'] = [
+  {
+    pool_name: 'test',
+    jndi_name: 'java:jboss/datasources/test',
+    server: '127.0.0.1',
+    port: '5432',
+    db_name: 'test',
+    db_user: 'test_user',
+    db_pass: 'test_pass',
+    pool_min: '5',
+    pool_max: '20'
+  },
+  {
+    pool_name: 'test2',
+    jndi_name: 'java:jboss/datasources/test2',
+    server: '127.0.0.1',
+    port: '5432',
+    db_name: 'test2',
+    db_user: 'test_user',
+    db_pass: 'test_pass',
+    pool_min: '5',
+    pool_max: '20'
+  }
+]
---
+++ wildfly/metadata.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+name             'wildfly'
+maintainer       'Brian Dwyer - Intelligent Digital Services'
+maintainer_email 'bdwyertech'
+license          'Apache License, Version 2.0'
+description      'Installs/Configures wildfly'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.4.0'
+
+supports 'centos'
+
+depends 'apt'
+depends 'yum'
+depends 'java', '~> 1.22'
+
+source_url 'https://github.com/bdwyertech/chef-wildfly' if respond_to?(:source_url)
+issues_url 'https://github.com/bdwyertech/chef-wildfly/issues' if respond_to?(:issues_url)
